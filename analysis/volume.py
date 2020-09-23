ceba273ac359458e47b7a0a65f989a68bc89f3b7
Jordan Blackadar <jordan.blackadar@outlook.com>
2020-09-23 01:13:38 -0400

    Update volume.py

--- analysis/volume.py
+++ analysis/volume.py
@@ -1,12 +1,12 @@
 import numpy as np
 
 
-def mask(arr, voxel_size=0.22):
+def mask(arr, voxel_size=1):
     """
     Calculates the volume of a slice (or series) based on a voxel size.
-    :param arr:
-    :param voxel_size:
-    :return:
+    :param arr: np.ndarray of type bool
+    :param voxel_size: Size of a voxel, in your unit of choice
+    :return: Single value, total volume (slice or volume-wise)
     """
     assert type(arr) is np.ndarray, "Array must be an n-dimensional numpy array."
     assert arr.dtype == np.bool or arr.dtype == bool, f"Array must contain boolean values."
